b34300030a1c600cc871585d5df48840cd177276
Lavreen <lszymo@gmail.com>
2020-10-21 22:15:32 +0200

    Initial commit

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+
+    public static void main (String[] args){
+        Greetings greetings = new Greetings();
+        System.out.println(greetings.sayHello(args.length>0?args[0]:null));
+    }
+}
